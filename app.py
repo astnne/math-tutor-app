0056fb5a840e559dd5f9c44114adcc7b0286dfb7
astnne <astnne09@gmail.com>
2024-01-14 18:56:07 -0800

    commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, flash, redirect, url_for, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return redirect("/home")
+
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
