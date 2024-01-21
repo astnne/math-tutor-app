cbb9f9f11001c95e53df755534bbe07f032e3674
astnne <astnne09@gmail.com>
2024-01-21 12:50:45 -0800

    commit

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return redirect("/index")
-
-@app.route("/index")
-def index():
     return render_template("index.html")
 
 @app.route("/contact")
@@ -15,4 +11,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8800)
+    app.run(debug=True)
